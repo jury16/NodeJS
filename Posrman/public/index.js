7553ce07901d6b77b8a21973d28473357f5edb64
Yurii <jury16@gmail.com>
2021-10-31 23:37:43 -0400

    Add new and appload fixed files

--- Posrman/public/index.js
+++ Posrman/public/index.js
@@ -1,105 +1,106 @@
 async function bodyQuise(){
-let form = document.getElementById('form');
-let divParams = document.getElementById('params');
-let divBody = document.getElementById('body');
-let divHeaders = document.getElementById('headers');
-let pUrl = document.getElementById('pUrl');
-let pMethod = document.getElementById('pMethod');
-let pParams = document.getElementById('pParams');
-let pHeaders = document.getElementById('pHeaders');
-let pBody = document.getElementById('pBody');
-let spanErr = document.getElementById('urlError');
-let headersBody = document.getElementById('headersBody');
-$("form").on( "submit", function( event ) {
-    event.preventDefault();
-    let paramsReq = {};
-    let headersReq = {};
-    let hashReq = [];    
-    let inputsParams = document.querySelectorAll('.params'); 
-    let inputsHeaders = document.querySelectorAll('.headers'); 
-    let inputsBody = document.querySelectorAll('.body');     
-    let methodReq;
-    let urlReq = event.target.url.value;
-    let bodyReq = event.target.body.value;
-    let getReq = event.target.get.checked;
-    getReq?methodReq = 'GET': methodReq = 'POST';
-    hashReq = {'url': urlReq, 'method':methodReq, 'params':paramsReq, 'headers':headersReq, 'body': bodyReq};
-    console.log(bodyReq)
-    getData (headersReq, inputsHeaders);
-    getData (paramsReq, inputsParams);
-    function getData(nameField, nameReq) {
-        for(let i = 0; i < nameReq.length / 2; i++){
-            nameField[nameReq[2*i].value] = nameReq[2*i+1].value;
+    let form = document.getElementById('form');
+    let divParams = document.getElementById('params');
+    let divBody = document.getElementById('body');
+    let divHeaders = document.getElementById('headers');
+    let pUrl = document.getElementById('pUrl');
+    let pMethod = document.getElementById('pMethod');
+    let pParams = document.getElementById('pParams');
+    let pHeaders = document.getElementById('pHeaders');
+    let pBody = document.getElementById('pBody');
+    let spanErr = document.getElementById('urlError');
+    let headersBody = document.getElementById('headersBody');
+    $('input[name="bodyType"]').on('click', function(event) {
+        event.target.id == 'none'? divBody.setAttribute('class','body _hiden'):divBody.setAttribute('class','body _active');    
+    });
+    $("form").on( "submit", function( event ) {
+        event.preventDefault();
+        let paramsReq = {};
+        let headersReq = {};
+        let hashReq = []; 
+        let bodyParam = ''   ;
+        let inputsParams = document.querySelectorAll('.params'); 
+        let inputsHeaders = document.querySelectorAll('.headers'); 
+        let inputsBodyReq = event.target.bodyType; 
+        for (let i = 0; i < inputsBodyReq.length; i++){
+            if (inputsBodyReq[i].checked == true){
+                bodyParam = inputsBodyReq[i].id;
+                break;
+            }
+        } 
+        let methodReq;
+        let urlReq = event.target.url.value;
+        let bodyReq = event.target.body.value;
+        let getReq = event.target.get.checked;
+        getReq?methodReq = 'GET': methodReq = 'POST';
+        hashReq = {'url': urlReq, 'method':methodReq, 'params':paramsReq, 'headers':headersReq, bodyType: bodyParam , body: bodyReq};
+        //console.log(('te"st').replace('"', ""));
+        getData (headersReq, inputsHeaders);
+        getData (paramsReq, inputsParams);
+        function getData(nameField, nameReq) {
+            for(let i = 0; i < nameReq.length / 2; i++){
+                nameField[nameReq[2*i].value] = nameReq[2*i+1].value;
+            }
         }
+        
+        if(urlReq){
+            $.ajax(('/request'),{ 
+                type:'POST', 
+                'Content-type': 'application/json',
+                "Access-Control-Allow-Origin" :"*",
+                //dataType: "json",
+                data: (hashReq),
+                success: dataLoaded(), 
+                //error:errorHandler
+            });
+            function dataLoaded(){
+                let divData = document.getElementById('storeData');
+                let title = document.getElementById('title');
+                title.innerHTML = 'Data to transfer'
+                pUrl.innerHTML = `URL:  ${urlReq}`;
+                spanErr.innerHTML = ``;
+                pMethod.innerHTML = `Method: ${methodReq}`;
+                paramsReq?pParams.innerHTML = `Params: ${JSON.stringify(paramsReq)}`: paramsReq = {};
+                headersReq?pHeaders.innerHTML = `Headers: ${JSON.stringify(headersReq)}`: headersReq = {};
+                pBody.innerHTML = `Body: ${bodyReq}`;
+            }                  
+            function errorHandler(jqXHR,statusStr,errorStr) {
+                alert(statusStr+' '+errorStr);
+            }
+        }
+        else {
+            spanErr.innerHTML = `Cann't be empty!`;
+        }
+        
+    }); 
+    let createInput = (name, nameClass) =>{
+        name.classList.add(nameClass);
+        name.name = name;
+        name.type = 'text';
+        name.size = '50';    
     }
-    if(urlReq){
-        $.ajax(('/request'),{ 
-            type:'POST', 
-            'Content-type': 'application/json',
-            //dataType: "json",
-            data: (hashReq),
-            success: dataLoaded(), 
-            //error:errorHandler
-        });
-        function dataLoaded(){
-            let divData = document.getElementById('storeData');
-            pUrl.innerHTML = `URL:  ${urlReq}`;
-            spanErr.innerHTML = ``;
-            pMethod.innerHTML = `Method: ${methodReq}`;
-            /*
-            !paramsReq?pParams.innerHTML = `Params: null`:pParams.innerHTML = `Params: ${paramsReq}`;
-            !headersReq?pHeaders.innerHTML = `Headers: null`:pHeaders.innerHTML = `Headers: ${headersReq}`;
-            !bodyReq?pBody.innerHTML = `Body: null`:pBody.innerHTML = `Body: ${bodyReq}`;
-            */
-        }                  
-    function errorHandler(jqXHR,statusStr,errorStr) {
-        alert(statusStr+' '+errorStr);
+    
+    $("#paramsButton").on( "click", function( event ) {
+        let paramsKey = document.createElement('input');
+        let divNewParamsFields = document.createElement('div');
+        let paramsValue = document.createElement('input');
+        createInput(paramsKey, 'params');
+        createInput(paramsValue, 'params');   
+        divNewParamsFields.appendChild(paramsKey);
+        divNewParamsFields.appendChild(paramsValue);
+        divNewParamsFields.appendChild(paramsValue);
+        divParams.appendChild(divNewParamsFields);
+    }); 
+    $("#headersButton").on( "click", function( event ) {
+        let headersKey = document.createElement('input');
+        let divNewHeaderFields = document.createElement('div');
+        let headersValue = document.createElement('input');
+        createInput(headersKey, 'headers');
+        createInput(headersValue, 'headers');   
+        divNewHeaderFields.appendChild(headersKey);
+        divNewHeaderFields.appendChild(headersValue);
+        divHeaders.appendChild(divNewHeaderFields);
+    });
+    
     }
-    }
-    else {
-        spanErr.innerHTML = `Cann't be empty!`;
-    }
-}); 
-let createInput = (name, nameClass) =>{
-    name.classList.add(nameClass);
-    name.name = name;
-    name.type = 'text';
-    name.size = '50';    
-}
-$("#bodyButtonChange").on( "click", function( event ) {
-    if (divBody.className === 'body _active'){        
-        divBody.className = 'body _hiden';
-        headersBody.className = '_active';
-    }
-    else {
-        console.log('here')
-        divBody.className = 'body _active';
-        headersBody.className = '_hiden';
-    }
-});
-$("#paramsButton").on( "click", function( event ) {
-    let paramsKey = document.createElement('input');
-    let divNewParamsFields = document.createElement('div');
-    let paramsValue = document.createElement('input');
-    createInput(paramsKey, 'params');
-    createInput(paramsValue, 'params');   
-    divNewParamsFields.appendChild(paramsKey);
-    divNewParamsFields.appendChild(paramsValue);
-    divNewParamsFields.appendChild(paramsValue);
-    divNewParamsFields.appendChild(paramsValue);
-    divNewParamsFields.appendChild(paramsValue);
-    divParams.appendChild(divNewParamsFields);
-}); 
-$("#headersButton").on( "click", function( event ) {
-    let headersKey = document.createElement('input');
-    let divNewHeaderFields = document.createElement('div');
-    let headersValue = document.createElement('input');
-    createInput(headersKey, 'headers');
-    createInput(headersValue, 'headers');   
-    divNewHeaderFields.appendChild(headersKey);
-    divNewHeaderFields.appendChild(headersValue);
-    divHeaders.appendChild(divNewHeaderFields);
-});
-
-}
-bodyQuise()
+    bodyQuise()
